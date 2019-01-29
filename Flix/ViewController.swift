1545451fee70832b5c17dec8e2696f8ffdce4823
Haiming WU <haiminw@uci.edu>
2019-01-28 20:04:25 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Haiming Wu on 1/28/19.
+//  Copyright © 2019 Haiming Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
