4f8d4a50e7b36294cf5c1db06c9ec25f5497b4b3
Haiming WU <haiminw@uci.edu>
2019-01-30 01:55:56 -0800

    Flix part 2.

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Haiming Wu on 1/30/19.
+//  Copyright © 2019 Haiming Wu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+   
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
